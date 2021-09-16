e418e53bc4124df37c7728973add894bf1e9119e
Klothie <89172306+Lab-Kloth@users.noreply.github.com>
2021-09-16 01:12:17 -0400

    Add files via upload

---
+++ random website picker.py
@@ -0,0 +1,17 @@
+#random verison 
+import webbrowser
+from random import choice
+pages = ['https://www.tiktok.com/@klothie?', 'https://www.instagram.com/klothiethegawd/', 'https://github.com/Lab-Kloth']
+webbrowser.open(choice(pages))
+
+# spam verison
+import webbrowser
+from random import choice
+words = ["https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?", "https://www.tiktok.com/@klothie?", "https://www.tiktok.com/@klothie?", "https://www.tiktok.com/@klothie?", "https://www.tiktok.com/@klothie?", "https://www.tiktok.com/@klothie?"]
+
+for word in words:
+    
+    webbrowser.open(word)
+else:
+
+    print("Finished social spam")   
