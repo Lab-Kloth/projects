f8e6f7b7c712cf48dba50372ca473a04fcf4eaf1
Klothie <89172306+Lab-Kloth@users.noreply.github.com>
2021-09-16 00:44:22 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+num1 = float(input("Enter a number: "))
+op = (input("""
++ = +(add)
+- = -(sub)
+/ = /(div)
+* = *(multi)
+
+Enter operator: 
+"""))
+num2 = float(input("Enter a number: "))
+
+if op == "+":
+    print(float(num1 + num2))
+elif op == "_":
+    print(float(num1 - num2))
+elif op == "/":
+    print(float(num1 / num2))
+elif op == "*":
+    print(float(num1 * num2))
+else:
+    print("invalid operator")
