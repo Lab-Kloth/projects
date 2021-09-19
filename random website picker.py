e48ead3e7e96117774bb9476582db243594f5028
Klothie <89172306+Lab-Kloth@users.noreply.github.com>
2021-09-19 00:40:13 -0400

    Update random website picker.py

--- random website picker.py
+++ random website picker.py
@@ -5,14 +5,8 @@ pages = ['https://www.tiktok.com/@klothie?', 'https://www.instagram.com/klothiet
 webbrowser.open(choice(pages))
 
 # spam verison
-# you can do a loop for the website i just didnt
+# !WARNING! this small script loops the url meaning it well open (url) 100 times beforing dieing wouldnt recommend using on your self
+# any harm done is your fault
 import webbrowser
-from random import choice
-words = ["https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?","https://www.tiktok.com/@klothie?", "https://www.tiktok.com/@klothie?", "https://www.tiktok.com/@klothie?", "https://www.tiktok.com/@klothie?", "https://www.tiktok.com/@klothie?", "https://www.tiktok.com/@klothie?"]
-
-for word in words:
-    
-    webbrowser.open(word)
-else:
-
-    print("Finished social spam")   
+for i in range(100):
+    webbrowser.open("https://www.tiktok.com/@klothie?") 
